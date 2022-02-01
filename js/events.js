b6abfd7d0033d6550fb5482f37fe712c28eab161
VitorBarbon <dev_vitorbarbon@gmail.com>
2022-01-31 23:11:01 -0300

    uptade menu hamburguer

--- js/events.js
+++ js/events.js
@@ -1,3 +1,7 @@
+const ulNav = document.querySelector('ul');
+const buttonOpen = document.querySelector('.fas');
+
+
 document.addEventListener('click', e => {
   const el = e.target;
   const containerPalette = document.getElementById('palettes-content');
@@ -7,4 +11,40 @@ document.addEventListener('click', e => {
     containerPalette.innerHTML = '';
     containerButtonReset.innerHTML = '';
   }
+
+  if (el.classList.contains('fas')) {
+    function setMenuHamburguer() {
+      if (el.classList.contains('fa-bars')) {
+        visibleMenuHamburguer([
+          'navbar-links-hidden',
+          'navbar-links',
+          'fa-bars',
+          'fa-times'
+        ]);
+      } else {
+        visibleMenuHamburguer(withOutHamburguer);
+      }
+    }
+    setMenuHamburguer();
+  }
 });
+
+function visibleMenuHamburguer([class1, class2, class3, class4]) {
+  ulNav.classList.remove(class1);
+  ulNav.classList.add(class2);
+  buttonOpen.classList.remove(class3);
+  buttonOpen.classList.add(class4);
+}
+
+const withOutHamburguer = [
+  'navbar-links',
+  'navbar-links-hidden',
+  'fa-times',
+  'fa-bars'
+];
+
+function removeMenuHamburguer() {
+  if (window.innerWidth > 820) {
+    visibleMenuHamburguer(withOutHamburguer);
+  }
+}
